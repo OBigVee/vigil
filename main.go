e2bbd92f5efe6296c52b5189dd43721ab21e5f3f
obigvee <adedejivictor98@gmail.com>
2026-04-18 20:53:49 +0100

    increase output list of processes

--- main.go
+++ main.go
@@ -6,10 +6,65 @@ import (
 	"sort"
 	"time"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/shirou/gopsutil/v3/cpu"
 	"github.com/shirou/gopsutil/v3/mem"
 	"github.com/shirou/gopsutil/v3/process"
 )
+
+type Process struct {
+		PID int32
+		Name string
+		MemoryPercent float32
+	}
+	
+type SystemState struct {
+	CPUUsage float64
+	UsedRAM uint64
+	TotalRAM uint64
+	RAMPercent float64
+	Processes []Process
+}
+
+
+type model struct {
+	state SystemState
+}
+
+// runs once when the app starts
+func (m model) Init() tea.Cmd{
+	return nil
+}
+
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd){
+	switch msg := msg.(type){
+		case tea.KeyMsg:
+			switch msg.String(){
+			case "q", "ctrl+c":
+				return m, tea.Quit
+			}
+	}
+	return m, nil
+}
+
+// view draws state to the terminal
+func (m model) View() string {
+	s := "==== Vigil: Terminal UI ====\n\n"
+	s += fmt.Sprintf("CPU Usage: %.2f%%\n", m.state.CPUUsage)
+	s += fmt.Sprintf("RAM Usage: %v MB / %v MB (%.2f%%)\n\n", m.state.UsedRAM, m.state.TotalRAM, m.state.RAMPercent)
+
+	s += "--- Top 10 processes by Memory ---\n"
+	for _, p := range m.state.Processes {
+		s += fmt.Sprintf("PID: %-8d | Mem: %5.2f%% | Name: %s\n", p.PID, p.MemoryPercent, p.Name)
+	}
+
+	s += "\n[Press 'q' or 'ctrl+c' to quit]\n"
+	return s
+}
+
+
+
 
 func main(){
 	fmt.Println("==== Vigil: initiated ===\n")
@@ -70,7 +125,7 @@ func main(){
 		
 	// print top 10 (or few , if < 10 are running)
 	
-	for i := 0; i < 10 && i < len(procList); i++ {
+	for i := 0; i < 200 && i < len(procList); i++ {
 		p := procList[i]
 		fmt.Printf("PID: %-8d | Mem: %5.2f%% | Name: %s\n", p.PID, p.MemoryPercent, p.Name)
 	}
